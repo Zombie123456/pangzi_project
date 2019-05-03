e5056a107b9bd6763501dd7edabbdc5774c5759a
chalice <chalice@unnotech.com>
2019-05-03 17:59:06 +0800

    d

--- stacks/deploy_stacks.sh
+++ stacks/deploy_stacks.sh
@@ -1,5 +1,5 @@
-docker stack deploy -c stack-demo-master-db.yml --with-registry-auth chalice_ser
+docker stack deploy -c stack-mathtest-master-db.yml --with-registry-auth chalice_ser
 docker stack deploy -c stack-message-broker.yml --with-registry-auth chalice_ser
-docker stack deploy -c stack-demo.yml --with-registry-auth chalice_ser
-docker stack deploy -c stack-demo-load-balancer.yml --with-registry-auth chalice_ser
+docker stack deploy -c stack-mathtest.yml --with-registry-auth chalice_ser
+docker stack deploy -c stack-mathtest-load-balancer.yml --with-registry-auth chalice_ser
 docker stack deploy -c stack-auto-deploy.yml --with-registry-auth chalice_ser
